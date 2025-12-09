10292de41f211ef8e20b416b1e49647f5c265652
Muhammad Shehu Bello <muhammad.shehu.bello1@gmail.com>
2025-12-09 19:32:15 +0000

    Create a Step by Step plan to fully  update, enhance, and upgrade the La

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,6 +71,9 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      fontWeight: {
+        'heavy': '900'
+      },
       keyframes: {
         'accordion-down': {
           from: {
